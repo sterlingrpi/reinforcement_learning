93b347a18ebf1fc2de6cc764a6eba18b4a990e68
snesbitt <sterling.nesbitt@gmail.com>
2020-02-18 10:14:12 -0500

    adding completed assignment

---
+++ completed/play.py
@@ -0,0 +1,27 @@
+import gym
+from completed.agents import RandomAgent
+from completed.agents import ValueIterAgent
+import numpy as np
+
+env = gym.make('FrozenLake-v0')
+gamma = 1
+#agent = RandomAgent(env.action_space)
+agent = ValueIterAgent(env,gamma)
+
+agent.value_iteration();
+agent.extract_policy();
+
+print("Agent Policy: ", agent.policy)
+all_rewards=[]
+for _ in range(1000):
+	obs=env.reset()
+	total_reward = 0
+	while True:
+		#env.render()
+		action = agent.choose_action(obs)
+		obs,reward,done,info = env.step(action)
+		if done:
+			all_rewards.append(reward)
+			break
+
+print("Average Reward: ", np.mean(all_rewards))
