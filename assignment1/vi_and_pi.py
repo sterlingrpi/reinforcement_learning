4e9008c9bcd5b2b873e2738f380ab3db508c18f0
snesbitt <sterling.nesbitt@gmail.com>
2020-02-21 10:37:32 -0500

    implemented value iteration to find optimal policy

--- assignment1/vi_and_pi.py
+++ assignment1/vi_and_pi.py
@@ -14,13 +14,29 @@ def policy_improvement(P, nS, nA, value_from_policy, policy, gamma=0.9):
 	return new_policy
 
 def policy_iteration(P, nS, nA, gamma=0.9, tol=10e-3):
-	value_function = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+	value_function = np.zeros(nS)
 	policy = np.random.randint(low=0, high=4, size=nS, dtype=int)
 	return value_function, policy
 
-def value_iteration(P, nS, nA, gamma=0.9, tol=1e-3):
-	value_function = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+def value_iteration(P, nS, nA, gamma, tol):
+	value_function = np.zeros(nS)
 	policy = np.random.randint(low=0, high=4, size=nS, dtype=int)
+	for i in range(1000):#find optimal value function
+		value_function_prev = value_function
+		for s in range(nS):
+			q_value = []
+			for a in range(nA):
+				next_states_rewards = []
+				for prob, next_s, reward, done in P[s][a]:
+					next_states_rewards.append((prob * (reward + gamma * value_function_prev[next_s])))
+				q_value.append(sum(next_states_rewards))
+			value_function[s] = max(q_value)
+	for s in range(nS):#extract policy using optimal value function
+		q_sa = np.zeros(nA)
+		for a in range(nA):
+			for prob, next_s, reward, done in P[s][a]:
+				q_sa[a] += (prob * (reward + gamma * value_function[next_s]))
+		policy[s] = np.argmax(q_sa)
 	return value_function, policy
 
 def render_single(env, policy, max_steps):
@@ -42,6 +58,10 @@ def render_single(env, policy, max_steps):
 
 if __name__ == "__main__":
 	env = gym.make("Deterministic-4x4-FrozenLake-v0")
+	print('env.P =', env.P)
+	print('env.P[s][a] =', env.P[0][0])
+	print('env.nS =', env.nS)
+	print('env.nA =', env.nA)
 	print("\n" + "-"*25 + "\nBeginning Policy Iteration\n" + "-"*25)
 	V_pi, p_pi = policy_iteration(env.P, env.nS, env.nA, gamma=0.9, tol=1e-3)
 	render_single(env, p_pi, max_steps=10)
