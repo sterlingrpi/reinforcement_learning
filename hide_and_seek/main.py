730ea42c3df32bfbd6c6fa166e77d1fa20f6763e
Sterling <sterling.nesbitt@gmail.com>
2020-08-08 09:49:46 -0400

    changing name from scenario to episode since this is the correct terminology

--- hide_and_seek/main.py
+++ hide_and_seek/main.py
@@ -4,12 +4,12 @@ from hide_and_seek import agent
 ob_size = 3
 map_size = 6
 max_steps = 32
-num_scenarios = 1000
+num_episodes = 1000
 gamma = 0.95
 epsilon = 0.25
 
 agent = agent.dqn_agent(max_steps, ob_size)
-for scenario in range(num_scenarios):
+for episode in range(num_episodes):
     env = environment.env(map_size, ob_size)
     for step in range(max_steps):
         ob = env.get_ob()
