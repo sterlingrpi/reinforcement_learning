4981fad02e86784a2d2bac49b1c1f204177ab6e9
Sterling <sterling.nesbitt@gmail.com>
2020-08-17 19:26:17 -0400

    adding the pygame directory for reinforcement learning in a 3D environment

---
+++ squirrel_hunter/main.py
@@ -0,0 +1,100 @@
+from multiprocessing import Pool
+import pygame
+import tkinter as tk
+import sys
+import os
+from squirrel_hunter import map
+from squirrel_hunter import SETTINGS
+from squirrel_hunter import player
+from squirrel_hunter import raycast_mp
+from squirrel_hunter import render
+from squirrel_hunter import npc
+import numpy as np
+from squirrel_hunter import reinforcement_learning
+
+if __name__ == '__main__':
+
+    os.environ['SDL_VIDEO_WINDOW_POS'] = f'{(tk.Tk().winfo_screenwidth() - SETTINGS.WIDTH) // 2},' \
+                                         f'{(tk.Tk().winfo_screenheight() - SETTINGS.HEIGHT) // 4}'
+    pygame.init()
+    pygame.display.set_caption('Ray casting')
+
+    sc = pygame.display.set_mode(SETTINGS.SIZE)
+    sc_map = pygame.Surface(SETTINGS.map_size)
+
+    clock = pygame.time.Clock()
+    pygame.mouse.set_visible(False)
+
+    map = map.Map()
+    render = render.Render(sc, sc_map)
+    agent = reinforcement_learning.agent(ob_shape=(512, 512), num_actions=4, load_weights=False, file_path='dqn_model.h5')
+
+    npc_0 = npc.NPC(sc, (700, 1270), 'bruda')
+    npc_1 = npc.NPC(sc, (700, 570), 'bruda_1')
+    npc_2 = npc.NPC(sc, (1400, 554), 'bruda_2')
+    #npc_3 = npc.NPC(sc, (721, 841), 'bruda_3')
+    #npc_4 = npc.NPC(sc, (719, 595), 'bruda_4')
+
+    player = player.Player(npc, clock, map.flat_map)
+    ray_cast = raycast_mp.RayCast(map.map)  # , sc, render)
+
+    with Pool(processes=4) as pool:
+
+        game = True
+        while game:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        game = False
+
+            sc.fill(SETTINGS.BLACK)
+            render.draw_background(player.angle_inf, player.real_x, player.real_y)
+
+            res_1 = pool.apply_async(ray_cast.raycasting, (player.angle, player.real_x, player.real_y, 0))
+            res_2 = pool.apply_async(ray_cast.raycasting, (player.angle, player.real_x, player.real_y, 1))
+            res_3 = pool.apply_async(ray_cast.raycasting, (player.angle, player.real_x, player.real_y, 2))
+            res_4 = pool.apply_async(ray_cast.raycasting, (player.angle, player.real_x, player.real_y, 3))
+
+            walls_1, floor_1 = res_1.get()
+            walls_2, floor_2 = res_2.get()
+            walls_3, floor_3 = res_3.get()
+            walls_4, floor_4 = res_4.get()
+
+            walls_1.update(walls_2); floor_1.update(floor_2)
+            walls_1.update(walls_3); floor_1.update(floor_3)
+            walls_1.update(walls_4); floor_1.update(floor_4)
+
+            render.draw_world(walls_1, floor_1)
+
+
+            npc_1.draw(player.angle, player.player_coords, walls_1, True)
+            npc_2.draw(player.angle, player.player_coords, walls_1, True)
+            #npc_3.draw(player.angle, player.player_coords, walls_1, True)
+            #npc_4.draw(player.angle, player.player_coords, walls_1, True)
+            npc_0.draw(player.angle, player.player_coords, walls_1, False)
+
+            #render.display_fps(clock)
+
+            # do the reinforcement learning things
+            '''
+            ob = render.get_ob(sc)
+            reward = render.get_reward(ob)
+            agent.give_reward(reward)
+            action = agent.get_action(ob, epsilon=0)
+
+            render.draw_map(map.map_of_tiles, player.real_x, player.real_y, player.sin_a, player.cos_a, ob[0] * 200)
+            for i in range(10):
+                if np.count_nonzero(ob == i) > 100 and i != 0:
+                    print(i)
+            '''
+            action = 'd'
+            player.movement(action)
+
+            # pygame.display.update()
+            pygame.display.flip()
+            # clock.tick()
+    pygame.quit()
+    sys.exit()
---
+++ squirrel_hunter/reinforcement_learning.py
@@ -0,0 +1,80 @@
+from tensorflow.keras.layers import LSTM, Dropout, BatchNormalization, Dense, Flatten, TimeDistributed
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras import Input, Model
+import numpy as np
+
+class agent:
+    def __init__(self, ob_shape, num_actions, load_weights=False, file_path='dqn_model.h5'):
+        input = Input(batch_shape=(1, 1, ob_shape[0], ob_shape[1]))
+        x = TimeDistributed(Flatten())(input)
+        x = LSTM(units=32, return_sequences=True, stateful=True)(x)
+        x = LSTM(units=32, return_sequences=True, stateful=True)(x)
+        x = LSTM(units=32, stateful=True)(x)
+        output = Dense(num_actions)(x)
+        model = Model(input, output)
+        model.compile(loss='mse', optimizer=Adam(lr=0.01))
+        self.Q_stateful = model
+
+        input = Input(shape=(None, ob_shape[0], ob_shape[1]))
+        x = TimeDistributed(Flatten())(input)
+        x = LSTM(units=32, return_sequences=True)(x)
+        x = LSTM(units=32, return_sequences=True)(x)
+        x = LSTM(units=32)(x)
+        output = Dense(num_actions)(x)
+        model = Model(input, output)
+        model.compile(loss='mse',optimizer=Adam(lr=0.01))
+        self.Q = model
+        self.Q_target = model
+
+        if load_weights:
+            self.Q_stateful.load_weights(file_path)
+            self.Q.load_weights(file_path)
+            self.Q_target.load_weights(file_path)
+
+        self.ob_shape = ob_shape
+        self.reset_memory()
+
+    def reset_memory(self):
+        self.obs = np.zeros(shape=(1, self.ob_shape[0], self.ob_shape[1]), dtype=np.float32)
+        self.actions = np.zeros(shape=1)
+        self.rewards = np.zeros(shape=1)
+        self.Q_stateful.reset_states()
+
+    def get_action(self, ob, epsilon):
+        self.obs = np.append(self.obs, [ob], axis=0)
+        if np.random.random() < epsilon:
+            action = np.random.randint(0, 4)
+        else:
+            action = np.argmax(self.Q(np.array([self.obs[1:]])))
+        self.actions = np.append(self.actions, [action], axis=0)
+        if action == 0:
+            action_wasd = 'w'
+        elif action == 1:
+            action_wasd = 'a'
+        elif action == 2:
+            action_wasd = 's'
+        elif action == 3:
+            action_wasd = 'd'
+        return action_wasd
+
+    def give_reward(self, reward):
+        self.rewards = np.append(self.rewards, [reward], axis=0)
+
+    def train(self, num_steps, gamma):
+        t = np.random.randint(1, num_steps + 1)
+        print('t =', t, 'num_steps =', num_steps)
+        obs_t = np.array([self.obs[1:t + 1]])
+        vals = self.Q_target.predict(obs_t)
+        print('vals before =', vals)
+        act_t = int(self.actions[t])
+        if t == num_steps:
+            vals[0, act_t] = self.rewards[t]
+        else:
+            obs_t_plus_1 = np.array([self.obs[1:t + 2]])
+            vals_target = self.Q_target.predict(obs_t_plus_1)
+            # vals[0, act_t] = vals[0, act_t] + alpha*(self.rewards[t] + gamma*np.amax(vals_target) - vals[0, act_t])
+            vals[0, act_t] = self.rewards[t] + gamma * np.amax(vals_target)
+        print('vals after =', vals)
+        self.Q.fit(obs_t, vals, verbose=0)
+        self.Q_stateful.set_weights(self.Q.get_weights())
+        self.reset_memory()
